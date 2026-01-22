2dbddfbba1c622afe61ec8fac87a767e69b929f9
Ubuntu <ubuntu@ip-172-31-10-67.eu-north-1.compute.internal>
2026-01-22 06:59:31 +0000

    commited to check ci-cd pipeline4

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!!!");
+        System.out.println("Hello GitHub!!!..");
         logStartup();
     }
    public static void logStartup() {
