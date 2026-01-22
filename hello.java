fdfdcaee4bc68923f076f35700d194e3296bd55d
Ubuntu <ubuntu@ip-172-31-34-67.eu-north-1.compute.internal>
2026-01-22 04:54:04 +0000

    Add startup from feature

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        logStartup();
+    }
+   public static void logStartup() {
+        System.out.println("Application started successfully");
     }
 }
 
